5e8b4625162ce5e97fd634250198b012c2cc18c3
Mulc1ber <dmo@mail.ru>
2024-12-04 16:17:04 +0300

    initial commit

---
+++ constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  JWT_SECRET: proccess.env.JWT_SECRET,
+};
---
+++ users.controller.js
@@ -0,0 +1,25 @@
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./constants");
+
+async function addUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  await User.create({ email, password: passwordHash });
+}
+
+async function loginUser(email, password) {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error("Invalid password");
+  }
+
+  return jwt.sign({ email }, JWT_SECRET, { expiresIn: "1h" });
+}
+
+module.exports = { addUser, loginUser };
